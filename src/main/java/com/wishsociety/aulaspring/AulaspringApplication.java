d61764de3340399538608997e884ca6aef3218c7
Rene Melo <renevitorf@gmail.com>
2025-02-10 15:29:18 -0300

    Created spring project

---
+++ src/main/java/com/wishsociety/aulaspring/AulaspringApplication.java
@@ -0,0 +1,13 @@
+package com.wishsociety.aulaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaspringApplication.class, args);
+	}
+
+}
